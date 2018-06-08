6667d34976998ddb573efd197d4ec6d504801b52
Abdallah <aibrahim@iotblue.net>
2018-06-09 00:41:33 +0200

    - task finished with the extra task

--- client/src/map_component.js
+++ client/src/map_component.js
@@ -1,115 +1,79 @@
 import React from 'react';
 import L from 'leaflet';
-import {Map, Marker, TileLayer, Tooltip} from 'react-leaflet';
-import MarkerClusterGroup from 'react-leaflet-markercluster';
-
-class MapComponent extends React.PureComponent {
-
+class MapComponent extends React.Component {
   constructor(props){
     super(props);
     this.state = {
       name: 'Loading...',
       coordinates: [0, 0],
-      clickedCoordinates: `Latlng of your click on map showed here ..`,
-      markers: [],
-      json: null,
+      distance: 'Click on map first!'
     };
   }
+  componentWillMount() {
 
-  setMarkerInfo(marker){
-    this.setState(({
-      name: marker.properties.name,
-      coordinates: marker.geometry.coordinates
-    }))
   }
 
-  getMarkers(rawData = this.state.json){
-    const markers = [];
-    rawData.forEach((marker, i) => {
-      markers.push(
-        <Marker position={marker.geometry.coordinates} key={i} onClick={() => this.setMarkerInfo(marker)}>
-          <Tooltip>
-            <p>{marker.properties.name}</p>
-          </Tooltip>
-        </Marker>
-    )
-    });
-    console.log('== markers ==', markers);
-    return markers;
+  handleClick(e){
+    fetch(`http://localhost:3000?lat=${e.latlng.lat}&lng=${e.latlng.lng}`)
+    .then(response => response.json())
+    .then(json => {
+      const { properties, geometry, distance } = json;
+        this.setState({
+          name: properties.name,
+          coordinates: geometry.coordinates,
+          distance: distance,
+        });
+    })
   }
-
-  getMapBounds() {
-    const {json} = this.state;
-    if (!json) {
-      return;
-    }
-    const markersCoors = json.map(station => station.geometry.coordinates);
-    const bounds = L.latLngBounds(markersCoors);
-    return Object.keys(bounds).length ? bounds : null;
-  };
-
-  getCoors(e){
-    console.log('== e ==', e);
-    this.setState(({
-      clickedCoordinates: `Your coordinates: lat:${e.latlng.lat}, lng:${e.latlng.lng}`,
-    }));
-  }
-
   componentDidMount() {
-    fetch('http://localhost:3000/stations')
+    fetch('http://localhost:3000')
       .then(response => response.json())
       .then((json) => {
-        console.log('=-= json[0] ==', json[0]);
-        const { properties, geometry } = json[0];
-        this.setState((({
-          json:json,
-          name:properties.name,
-          coordinates: geometry.coordinates
-        })));
-    });
-    // this.map = new L.Map('mapid');
-    // const osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-    // const attribution = 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
-    // const osm = new L.TileLayer(osmUrl, {
-    //   minZoom: 8,
-    //   maxZoom: 12,
-    //   attribution,
-    // });
-    // this.map.setView(new L.LatLng(52.51, 13.40), 9);
-    // this.map.addLayer(osm);
+        const { properties, geometry, distance } = json;
+        this.setState({
+          name: properties.name,
+          coordinates: geometry.coordinates,
+          distance:distance
+        });
+      })
+      .then( () => {
+        this.map = new L.Map('mapid');
+        const {name, coordinates} = this.state;
+        const osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+        const attribution = 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
+        const osm = new L.TileLayer(osmUrl, {
+          minZoom: 8,
+          maxZoom: 12,
+          attribution,
+        });
+        const center = coordinates || new L.LatLng(52.51, 13.40);
+        this.map.setView(center, 10);
+        this.map.addLayer(osm);
+        this.map.addEventListener('click', (e) => {
+          this.handleClick(e);
+        })
+        console.log(' ==== ', coordinates)
+        this.marker = new L.marker(coordinates).addTo(this.map);
+        this.marker.bindTooltip(name);
+      })
+  }
+  componentWillUpdate(nextProps, nextState){
+    if((this.state.coordinates[0] !== nextState.coordinates[0]) && this.marker){
+      this.marker.setLatLng(nextState.coordinates);
+      this.marker.bindTooltip(nextState.name);
+    }
   }
   render() {
-    const {name, coordinates, json, clickedCoordinates} = this.state;
-    const center = json && json[0].geometry.coordinates;
+    const {name, coordinates, distance} = this.state;
     return (
-      <React.Fragment>
-      <div id="info">
-      <p id="clickValue">{clickedCoordinates}</p>
-      <p>Name: {name}</p>
-        <p>Coordinates: lat: {coordinates[0]}, lng: {coordinates[1]}</p>
-      </div>
-      <Map
-        id="mapid"
-        center={center || [13.3454394, 52.5464403]}
-        style={{height:'400'}}
-        zoom={10}
-        bounds={this.getMapBounds()}
-        onClick={(e) => this.getCoors(e)}>
-        <TileLayer
-          attribution='Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
-          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-        />
-        <MarkerClusterGroup showCoverageOnHover={false}>
-          {json && this.getMarkers(json)}
-        </MarkerClusterGroup>
-      </Map>
-    </React.Fragment>
-
-        // <div>
-        //   Name: {this.state.name}<br />
-        //   {this.state.coordinates[1]} {this.state.coordinates[0]}
-        //   <div id="mapid"></div>
-        // </div>
+        <React.Fragment>
+          <div id="info">
+            <p>Distance in Meters: {distance} </p>
+            <p>Name: {name}</p>
+            <p>lat: {coordinates[1]}, lng: {coordinates[0]}</p>
+          </div>
+          <div id="mapid"></div>
+        </React.Fragment>
       );
   }
 };
--- server/index.js
+++ server/index.js
@@ -3,6 +3,33 @@ const app = express()
 
 const fuelStations = require('./fuelstations.json').features;
 
+
+// Converts numeric degrees to radians
+function toRad(Value)
+{
+    return Value * Math.PI / 180;
+}
+/**
+ * haversine formula
+ * lat1, lon1, lat2, lon2 integers
+ * returns distance in km
+ */
+ function calcCrow(lat1, lon1, lat2, lon2)
+ {
+   var R = 6371; // km
+   var dLat = toRad(lat2-lat1);
+   var dLon = toRad(lon2-lon1);
+   var lat1 = toRad(lat1);
+   var lat2 = toRad(lat2);
+
+   var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+     Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
+   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+   var d = R * c;
+   return d;
+ }
+
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -11,25 +38,21 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => {
   // TODO implement first server.
-  console.log('sending');
-  res.send(fuelStations[0])
+  const {query : {lat, lng}} = req;
+  let nearest = fuelStations[0];
+  let nearestDistance = 2000000;
+  if(lat && lng){
+    nearest = fuelStations.reduce((accum, station) => {
+      const distance = calcCrow(lat, lng, station.geometry.coordinates[0], station.geometry.coordinates[1])*1000
+      if (distance < nearestDistance ) {
+        nearestDistance = distance;
+        accum = station
+      }
+      return accum;
+    }, {})
+  }
+  nearest.distance = Math.round(nearestDistance *100) / 100;
+  res.send(nearest)
 })
-app.get('/nearest', (req, res) => {
-  // TODO implement first server.
-  // const {query} = req;
-  // const nearestStationIndex = Math.min(fuelStations.find(station => {
-  //   return [
-  //     Math.abs(station.geometry.coordinates[0] - query.lat),
-  //     Math.abs(station.geometry.coordinates[1] - query.lng)
-  //   ]
-  // }));
 
-  console.log('query', query);
-  console.log('== [nearestStationIndex] ==', nearestStationIndex);
-})
-app.get('/stations', (req, res) => {
-  // TODO implement first server.
-  console.log('sending');
-  res.send(fuelStations)
-})
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
